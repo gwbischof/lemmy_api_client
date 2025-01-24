adc7d4828297bcf3f2d3f25c9d823064af6a1733
Garrett Bischof <bischofgarrett@gmail.com>
2025-01-24 18:33:56 -0500

    update post model for request

--- lib/src/v3/models/post/post.dart
+++ lib/src/v3/models/post/post.dart
@@ -33,6 +33,12 @@ class Post with _$Post {
     required bool featuredLocal, // v0.18.0
     String? urlContentType, // v0.19.4 (optional)
     String? altText, // v0.19.4 (optional)
+    String? bid_id,
+    int? rating,
+    String? pickup_location,
+    int? pickup_time,
+    String? dropoff_location,
+    int? dropoff_time,
   }) = _Post;
 
   const Post._();
